95fffe27943c7224e8636773502dff3262861f5a
Neeraj Kumar <neeraj1.jnv@gmail.com>
2025-07-16 08:23:13 +0530

    added a storage account sa-patlu as required

--- code/main.tf
+++ code/main.tf
@@ -7,3 +7,11 @@ resource "azurerm_resource_group" "rg-git2" {
   name     = "rg-git2"
   location = "central India"
 }
+
+resource "azurerm_storage_account" "sa-patlu" {
+  name                     = "sa-patlu"
+  resource_group_name      = azurerm_resource_group.rg-git.name
+  location                 = azurerm_resource_group.rg-git.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
